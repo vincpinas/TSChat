4b26b0e61d76bf84f17cf8b696deb8699e2c165d
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-02-22 18:21:24 +0100

    :bug: IMPORT ERRORS FIXED

---
+++ Client/src/Components/Sidebar/SidebarChannel.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface sidebarChannelProps {
+    channelName: string;
+    channelLink: string;
+}
+
+function SidebarChannel(props: sidebarChannelProps) {
+    return (
+        <Link className="sidebarChannel" to={props.channelLink}>
+            <div className="channelName">
+                <span className="channelHashtag">#</span>
+                { props.channelName }
+            </div>
+        </Link>
+    )
+}
+
+export default SidebarChannel
