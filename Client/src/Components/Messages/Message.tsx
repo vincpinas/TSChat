05addae3826b30177089bb688beeef7d37fcd872
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-02-25 00:50:37 +0100

    :bug: MESSAGE PROPS FIX

--- Client/src/Components/Messages/Message.tsx
+++ Client/src/Components/Messages/Message.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+interface messageProps {
+    message: {
+        text: string;
+        user: string;
+    }
+}
 
-const Message = ({ message }: any) => {
+const Message = ({ message }: messageProps) => {
     return (
         <div className="messageContainer">
             <p className="messageSender">{message.user}</p>
